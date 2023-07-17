7033302f8973f244042f1ee47510d30810c21c97
Hammad Ahmed <hammad20@gmail.com>
2023-07-17 17:48:25 +0500

    First setup

---
+++ basic_ui.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Simple SVG Form</title>
+</head>
+<body>
+	<?php include("form_1200_plain.svg"); ?>
+	<form>
+		
+	</form>
+</body>
+</html>
